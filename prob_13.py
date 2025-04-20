38bb47196079c9aa0e64595437c1aaba840fd0fc
Krishna <mr.krishnamandloi@gmail.com>
2025-04-20 17:57:17 +0530

    dict program

--- prob_13.py
+++ prob_13.py
@@ -2,11 +2,19 @@
 sentence = 'hello world welcome to python programming hi there'
 #{'h':['hello', 'hai'], 'w':['world', 'welcome']......}
 
-d = {}
-for word in sentence.split():
-    if word[0] not in d:
-        d[word[0]] = [word]
-    else:
-        d[word[0]] += [word]
+# d = {}
+# for word in sentence.split():
+#     if word[0] not in d:
+#         d[word[0]] = [word]
+#     else:
+#         d[word[0]] += [word]
 
-print(d)
+# print(d)
+
+from collections import defaultdict
+dd = defaultdict(list)
+
+for ch in sentence.split():
+    dd[ch[0]] += [ch]
+
+print(dd)
